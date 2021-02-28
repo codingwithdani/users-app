553d6bfbdc3758c5df75bb78759354ffa00e6773
Daniela <aguileramalave@gmail.com>
2021-02-28 18:49:20 +0100

    add create user form

---
+++ src/components/CreateUserForm/index.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { Box, Form, FormField, TextInput } from 'grommet'
+
+import { ButtonStyled, FormContainer, ErrorMessage, SuccessfulMessage } from './styles'
+import { createNewsUser } from '../../services/createUser'
+
+const INITIAL_STATE = { name: '', job: '' }
+
+export const CreateUserForm = () => {
+  const [value, setValue] = useState(INITIAL_STATE)
+  const [errors, setErrors] = useState([])
+  const [success, setSuccess] = useState(false)
+
+  const handleInputChange = (nextValue) => {
+    setValue(nextValue)
+  }
+
+  const handleSubmit = () => {
+    createNewsUser(value).then(({ isValid, errors }) => {
+      setSuccess(isValid)
+      setErrors(errors)
+      if (isValid) setValue(INITIAL_STATE)
+    })
+  }
+  return (
+    <FormContainer pad='large' elevation='large' min-height='460px' justify='center' align='center' background='#ffffff' round='small'>
+      <Form
+        value={value}
+        onChange={handleInputChange}
+        onSubmit={handleSubmit}
+      >
+        <Box>
+          <FormField name='name' htmlFor='text-input-id' label='Name'>
+            <TextInput id='text-input-id' name='name' />
+          </FormField>
+        </Box>
+        <Box>
+          <FormField name='job' htmlFor='text-input-id' label='Job'>
+            <TextInput id='text-input-id' name='job' />
+          </FormField>
+        </Box>
+        {
+          errors.length
+            ? (
+              <Box justify='center' align='center'>
+                {errors.map(error => <ErrorMessage key={error}>{error}</ErrorMessage>)}
+              </Box>
+              )
+            : null
+        }
+        {
+          success &&
+          (
+            <Box justify='center' align='center' color='green'>
+              <SuccessfulMessage>User created successfully</SuccessfulMessage>
+            </Box>
+          )
+        }
+        <Box direction='row' justify='center' gap='medium' pad={{ vertical: 'medium' }}>
+          <ButtonStyled primary color='rgb(123, 127, 190)' type='submit' label='Create user' />
+        </Box>
+      </Form>
+    </FormContainer>
+  )
+}
---
+++ src/components/CreateUserForm/styles.js
@@ -0,0 +1,32 @@
+
+import styled from 'styled-components'
+
+import {
+  Box,
+  Button
+} from 'grommet'
+
+export const ButtonStyled = styled(Button)`
+    color: #ffff;
+    font-size: 14px;
+`
+
+export const ErrorMessage = styled.p`
+  color: red;
+  font-size: 12px;
+  margin-bottom: 4px;
+`
+
+export const SuccessfulMessage = styled.p`
+  color: green;
+  font-size: 12px;
+  margin-bottom: 4px;
+`
+
+export const FormContainer = styled(Box)`
+  color: gray;
+  font-size: 14px;
+  @media (min-width: 768px) {
+        min-width: 300px;
+    }
+`
